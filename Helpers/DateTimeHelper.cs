f44820514879abd57f5816cfedc91fcd491d3ee0
Kyle Rudd <kyleruddbiz@gmail.com>
2025-10-04 22:21:24 -0600

    Default to filescoped namespaces. Fix due date display formatting

---
+++ Helpers/DateTimeHelper.cs
@@ -0,0 +1,10 @@
+﻿namespace WhiteboardProjectBuilder.Helpers;
+
+public static class DateTimeHelper
+{
+    public static string ToMonthDayFormat(DateTime? dateTime)
+    {
+        return dateTime?.ToString("M/d")
+            ?? string.Empty;
+    }
+}
