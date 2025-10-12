6546c27c589b1f8e65db2e39cfcb1be24c8e0d77
Kyle Rudd <kyleruddbiz@gmail.com>
2025-10-12 16:12:42 -0600

    Change display of default image error

--- Converters/ArchivedBackgroundConverter.cs
+++ Converters/ArchivedBackgroundConverter.cs
@@ -1,22 +1,28 @@
-using Microsoft.UI.Xaml.Data;
 using Microsoft.UI.Xaml.Media;
-using Microsoft.UI;
 
 namespace WhiteboardProjectBuilder.Converters;
 
-public class ArchivedBackgroundConverter : IValueConverter
+public static class BackgroundColorHelper
 {
-    public object Convert(object value, Type targetType, object parameter, string language)
+    public static SolidColorBrush GetBackgroundBrush(bool isArchived, bool isUsingTemporaryImage)
     {
-        if (value is bool isArchived && isArchived)
+        if (isArchived && isUsingTemporaryImage)
         {
+            // Blended color: average of tan (245, 222, 179) and light red (255, 220, 220)
+            return new SolidColorBrush(Windows.UI.Color.FromArgb(255, 250, 221, 200));
+        }
+        else if (isArchived)
+        {
+            // Light tan for archived
             return new SolidColorBrush(Windows.UI.Color.FromArgb(255, 245, 222, 179));
         }
-        return new SolidColorBrush(Windows.UI.Color.FromArgb(255, 255, 255, 255));
-    }
+        else if (isUsingTemporaryImage)
+        {
+            // Light red for temporary image
+            return new SolidColorBrush(Windows.UI.Color.FromArgb(255, 255, 220, 220));
+        }
 
-    public object ConvertBack(object value, Type targetType, object parameter, string language)
-    {
-        throw new NotImplementedException();
+        // White for normal
+        return new SolidColorBrush(Windows.UI.Color.FromArgb(255, 255, 255, 255));
     }
 }
