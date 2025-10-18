330d22e68593ff7c5b23df0674cdf0572929fc1e
Kyle Rudd <kyleruddbiz@gmail.com>
2025-10-18 10:45:53 -0600

    Remove old models

--- Models/GoalItem.cs
+++
@@ -1,8 +0,0 @@
-namespace WhiteboardProjectBuilder.Models;
-
-public class GoalItem
-{
-    public required string Title { get; set; }
-    public string? Subtitle { get; set; }
-    public required string Image { get; set; }
-}
--- Models/InspirationItem.cs
+++
@@ -1,7 +0,0 @@
-namespace WhiteboardProjectBuilder.Models;
-
-public class InspirationItem
-{
-    public required string Text { get; set; }
-    public required string Image { get; set; }
-}
